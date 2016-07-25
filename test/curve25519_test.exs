caeb6b5d010c3f574117bd01ed073d554e10e4f9
Bram Verburg <brverbur@cisco.com>
2016-07-25 17:25:49 +0300

    add RFC7748 test vectors

--- test/curve25519_test.exs
+++ test/curve25519_test.exs
@@ -64,4 +64,19 @@ defmodule Curve25519Test do
     assert Curve25519.derive_shared_secret(short_key,short_key)   == :error
   end
 
+  test "RFC7748 test vectors" do
+    # Section 6.1
+    a = Base.decode16("77076d0a7318a57d3c16c17251b26645df4c2f87ebc0992ab177fba51db92c2a")
+    a_pub = Base.decode16("8520f0098930a754748b7ddcb43ef75a0dbf3a0d26381af4eba4a98eaa9b4e6a")
+    assert a_pub == Curve25519.derive_public_key(a)
+
+    b = Base.decode16("5dab087e624a8a4b79e17f8b83800ee66f3bb1292618b6fd1c2f8b27ff88e0eb")
+    b_pub = Base.decode16("de9edb7d7b7dc1b4d35b61c2ece435373f8343c85b78674dadfc7e146f882b4f")
+    assert b_pub == Curve25519.derive_public_key(b)
+
+    k = Base.decode16("4a5d9d5ba4ce2de1728e3bf480350f25e07e21c947d19e3376f09b3c1e161742")
+    assert k == Curve25519.derive_shared_secret(a, b_pub)
+    assert k == Curve25519.derive_shared_secret(b, a_pub)
+  end
+
 end
